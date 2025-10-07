d5910f67d8b7fdf91a4f279dda9a576f7d3f1a88
PedroVyttor <vyttinho18@gmail.com>
2025-10-06 21:35:46 -0300

    Upgrade

--- EXEMPLOS/Estrutura de Repetição 'while'/ex8.py
+++ EXEMPLOS/Estrutura de Repetição 'while'/ex8.py
@@ -1,10 +1,6 @@
-#parametros da função 'range', regredindo
-#range com 1 numero: condição de parada
-#rande com 2 numeros: valor inicial e condição de parada (invertido)
-#range com 3 numeros: valor inicial, condição de parada e pulo (invertido)
-
-#for x in range (7, 2, -1):
-#    print(x)
-
-for num in range (10, -12, -2):
-    print(num)
+idade = 0
+for i in range (2006, 2026):
+    if idade == 12:
+        continue
+    idade += 1
+    print(idade)
