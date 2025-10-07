bc3f9f2ea8c33ec986b81c3deee83e9d65be22cf
PedroVyttor <vyttinho18@gmail.com>
2025-10-07 15:33:27 -0300

    add

---
+++ ATIVIDADES/EstruturaDeRepeticao - PythonBrasil (Questoes)/quest7.py
@@ -0,0 +1,5 @@
+#  Faça um programa que leia 5 números e informe o maior número.
+soma = 0
+for num in range (1, 6):
+    n = int(input(f'digite o {num}º numero: '))
+
--- ATIVIDADES/EstruturaDeRepeticao - PythonBrasil (Questoes)/quest8.py
+++ ATIVIDADES/EstruturaDeRepeticao - PythonBrasil (Questoes)/quest8.py
@@ -3,9 +3,9 @@
 soma = 0
 
 for num in range(1, 6):
-    n = float(input(f"Digite o {num} número: "))
+    n = float(input(f"Digite o {num}º número: "))
     soma += n
 
-print(f"soma: {soma}")
+print(f"\nsoma: {soma}")
 print(f"media: {soma / 5}")
 
