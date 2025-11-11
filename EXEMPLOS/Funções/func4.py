35c518cf1c086f2eb38168eecf13af5f3e1a8834
PedroVyttor <vyttinho18@gmail.com>
2025-11-11 20:22:58 -0300

    validaremail

--- EXEMPLOS/Funções/func4.py
+++ EXEMPLOS/Funções/func4.py
@@ -1,1 +1,19 @@
+import random
 
+def validarEmail(email):
+    if '@' not in email:
+        print('Tá faltando o @ ai')
+        return False
+
+    if 'gmail' not in email:
+        print('Ta faltando o gmail')
+        return False
+
+    if '.com' not in email:
+        print('Ta faltando o .com')
+        return False
+
+email = input('Digite seu email: ')
+validarEmail(email)
+
+print(validarEmail(email))
