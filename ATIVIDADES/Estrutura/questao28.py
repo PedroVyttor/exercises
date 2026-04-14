c6507fcdf40b53b4535c6bf9140574338c71678d
PedroVyttor <vyttinho18@gmail.com>
2026-04-13 21:45:30 -0300

    a

---
+++ ATIVIDADES/Estrutura/questao28.py
@@ -0,0 +1,2 @@
+#escreva um programa que imprima qunatas vogais a,e,i,o,u  tem em uma palavra.
+#(Preste atenção no plural e no singular)
--- EXEMPLOS/Estrutura de Repetição 'for' (Exemplos)/ex11.py
+++ EXEMPLOS/Estrutura de Repetição 'for' (Exemplos)/ex11.py
@@ -1,18 +1,3 @@
-#escreva um programa que imprima quantas vogais a, e, i, o, u
-#tem em uma palavra (Atenção no plural e singular)
-
-plvr = input('Digite alguma coisa ai: ').lower()
-print(plvr)
-a = 0
-e = 0
-i = 0
-o = 0
-u = 0
-
-for letra in plvr:
-    print(letra)
-
-
 
 
 
